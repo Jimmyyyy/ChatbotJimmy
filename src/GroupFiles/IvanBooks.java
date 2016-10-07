09877c8d7cda4a3541e6dba46ece7b5c8bb74ab2
L0hL <izhen7813@bths.edu>
2016-10-06 22:38:35 -0400

    Update IvanBooks.java

--- src/GroupFiles/IvanBooks.java
+++ src/GroupFiles/IvanBooks.java
@@ -1,9 +1,13 @@
 package GroupFiles;
 
-	public class IvanBooks implements Chatbot{
+
+
+public class IvanBooks implements Chatbot{
 	//private static String response;
 	private boolean inMainLoop;
 	private boolean inBookLoop;
+	private boolean inOtherLoop;
+	private boolean inAntLoop;
 	//private boolean inUserLoop;
 	private boolean inBotLoop;
 	public String[] Genre = {"Fantasy", "Mystery", "Horror"};
@@ -14,8 +18,8 @@ package GroupFiles;
 	private String userGenre;
 	//public String[] Mystery = {"a", "b","c","d","e"};
 //	public String[] Horror = {"a", "b","c","d","e"};
-	public int yes = JimmyMain.findKeyword(readF, "yes", 0);
-	public int no = JimmyMain.findKeyword(readF, "no", 0);
+	//public int Yes = JimmyMain.findKeyword(readF, "yes", 0);
+	//public int No = JimmyMain.findKeyword(readF, "no", 0);
 	public void talk() {
 		inMainLoop = true;
 		while(inMainLoop) {
@@ -23,38 +27,41 @@ package GroupFiles;
 			JimmyMain.print("Do you like to read Fantasy ?");
 			while(inBookLoop){
 				readF = JimmyMain.promptInput();
-				if(yes>=0){
+				if(JimmyMain.findKeyword(readF, "yes", 0)>=0){
 					JimmyMain.print("Cool! "+ " That's my favorite genre.");
+					inBookLoop = false;
 					inBotLoop = true;
 				}
-				else if (no>=0){
+				else if (JimmyMain.findKeyword(readF, "no", 0)>=0){
 					JimmyMain.print("What genre do you like to read?");
+					inBookLoop = false;
+				}
+				while(inAntLoop){
 					userGenre = JimmyMain.promptInput();
+					JimmyMain.print("Oh, your favorite genre is" + userGenre +"." + "Your favorite genre is boring!" 
+							+ "How about we talk about Fantasy first? Let me recommend you some fantasy novels!");
+
+					if(JimmyMain.findKeyword(userGenre, "no", 0)>=0){
+						JimmyMain.print("Then I would rather not talk about books.");
+						JimmyMain.promptForever();
+					}
 				}
-				if(JimmyMain.findKeyword(userGenre, "fantasy", 0)>=0){
-					JimmyMain.print("You said no before!" + "Whatever, lets talk about fantasy");
-					inBotLoop = true;
-				}
-				else{
-					JimmyMain.print("Your favorite genre is boring!" + "How about you let me recommend you some fantasy novels?");
-				}
-				if(no>=0){
-					JimmyMain.print("Then I would rather not talk about books.");
-					JimmyMain.promptForever();
-				}
-				else if (yes>=0){
-					inBotLoop = true;
-				}
+
+			//	else if (JimmyMain.findKeyword(readF, "yes", 0)>=0){
+			//		inBotLoop = true;
+			//	}
 
 			}
 			while(inBotLoop){
 				JimmyMain.print("Do you like to read a series novel or a single novel?");
 				readF = JimmyMain.promptInput();
 				if(JimmyMain.findKeyword(readF, "seres", 0)>=0){
-					JimmyMain.print("I recommend you to read : " + Fantasy.toString() +" .") ;
+					JimmyMain.print("I recommend you to read : " + Fantasy[0].toString() +" .") ;
+					
 				}
 				else{
-					JimmyMain.print("I reccomend you to read: "+ Fantasy2.toString() + ".") ;
+					JimmyMain.print("I reccomend you to read: "+ Fantasy2[0].toString() + ".") ;
+					
 				}
 			}
 		}
